bf07116bc53bd46becb6836b817e3ed0b54cd8a5
Kalhara Gunawardhana <kalzmiami@gmail.com>
2026-05-31 19:30:22 +0530

    test: add test setup file

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,8 @@
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest'
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+
+// Automatic cleanup after each test
+afterEach(() => {
+    cleanup()
+})
